15041ce0d38325af52a4d45f7f906de4e11cd820
Fabricio Bezerra <contato@adawn.dev>
2020-12-02 14:08:44 -0300

    Day 01 benchmarking

--- day01/part01.rb
+++ day01/part01.rb
@@ -1,19 +1,29 @@
 # frozen_string_literal: true
 
+require 'benchmark'
+
 text = File.open(__dir__ + '/input.txt').read
 
 input = text.split("\n").map { |string| string.to_i }
 
-valueX = 0
-valueY = 0
+Benchmark.bm do |x|
+  x.report("Live code") do
+    valueX = 0
+    valueY = 0
 
-input.each do |x|
-  input.each do |y|
-    if x + y == 2020
-      valueX = x
-      valueY = y
+    input.each do |x|
+      input.each do |y|
+        if x + y == 2020
+          valueX = x
+          valueY = y
+        end
+      end
     end
+
+    puts valueX * valueY
+  end
+
+  x.report("Post Live Code") do
+    puts input.reduce(0) { |acc, value| input.include?(2020 - value) ? (2020 - value) * value : acc }
   end
 end
-
-puts valueX * valueY
--- day01/part02.rb
+++ day01/part02.rb
@@ -1,23 +1,35 @@
 # frozen_string_literal: true
 
+require 'benchmark'
+
 text = File.open(__dir__ + '/input.txt').read
 
 input = text.split("\n").map { |string| string.to_i }
 
-valueX = 0
-valueY = 0
-valueZ = 0
+Benchmark.bm do |x|
+  x.report("Live code") do
+    valueX = 0
+    valueY = 0
+    valueZ = 0
 
-input.each do |x|
-  input.each do |y|
-    input.each do |z|
-      if x + y + z == 2020
-        valueX = x
-        valueY = y
-        valueZ = z
+    input.each do |x|
+      input.each do |y|
+        input.each do |z|
+          if x + y + z == 2020
+            valueX = x
+            valueY = y
+            valueZ = z
+          end
+        end
       end
     end
+
+    puts valueX * valueY * valueZ
+  end
+
+  x.report("Post Live Code") do
+    permut = input.permutation(2)
+    puts permut.reduce(0) { |acc, value| input.include?(2020 - value.sum) ? (2020 - value.sum) * value.inject(:*) : acc }
   end
 end
 
-puts valueX * valueY * valueZ
